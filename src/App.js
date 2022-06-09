2e9eb45504e0ffd0cdcd066675b5de5d4899c204
JBxss <juanseb100@gmail.com>
2022-06-09 14:31:29 -0500

    [ADD] List of PhotoCard and Logo Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { GlobalStyle } from "./GlobalStyles";
 import { ListOfCategories } from "./components/ListOfCategories";
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+import { Logo } from "./components/Logo";
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 );
