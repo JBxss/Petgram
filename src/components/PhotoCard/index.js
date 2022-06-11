88edba804de0edf3ecb3246c711b2d788df20992
JBxss <juanseb100@gmail.com>
2022-06-10 19:52:50 -0500

    [ADD] Lazy load & Like Features

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,22 +1,68 @@
-import React from "react";
-import { ImgWrapper, Img, Button } from "./styles";
-import { MdFavoriteBorder } from "react-icons/md";
+import React, { useEffect, useRef, useState } from "react";
+import { Article, ImgWrapper, Img, Button } from "./styles";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 const DEFAULT_IMAGE =
   "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png";
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
+  const element = useRef(null);
+  const [show, setShow] = useState(false);
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key);
+      return like;
+    } catch (e) {
+      return false;
+    }
+  });
 
-      <Button>
-        <MdFavoriteBorder size="32px" />
-        {likes} likes!
-      </Button>
-    </article>
+  useEffect(
+    function () {
+      Promise.resolve(
+        typeof window.IntersectionObserver != "undefined"
+          ? window.IntersectionObserver
+          : import("intersection-observer")
+      ).then(() => {
+        const observer = new window.IntersectionObserver(function (entries) {
+          const { isIntersecting } = entries[0];
+          if (isIntersecting) {
+            setShow(true);
+            observer.disconnect();
+          }
+        });
+        observer.observe(element.current);
+      });
+    },
+    [element]
+  );
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
+  const setLocalStorage = (value) => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <Article ref={element}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+
+          <Button onClick={() => setLocalStorage(!liked)}>
+            <Icon size="32px" />
+            {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   );
 };
